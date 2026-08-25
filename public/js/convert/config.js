0d4e8edfe9eb3cc735ce26996ba6676620d2f40b
pmarcelacademy1 <pmarcelacademy@gmail.com>
2026-08-25 15:03:27 +0100

    well yes

--- public/js/convert/config.js
+++ public/js/convert/config.js
@@ -1,5 +1,6 @@
 const API_BASE_URL = 'https://pocketoptionss-backend.onrender.com';
 
+// const API_BASE_URL = 'http://127.0.0.1:3000';
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
